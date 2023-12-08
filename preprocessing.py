9c7af44b868f8d1b59dc2408c6869b817a12f8c9
AbdulrahmanMohamedTawfik <20201701098@cis.asu.edu.eg>
2023-12-08 20:52:43 +0200

    stemming, drop null, drop "" added

--- preprocessing.py
+++ preprocessing.py
@@ -3,15 +3,22 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-
+from nltk.stem import ISRIStemmer
 
 # uncomment this in first run
 # nltk.download('punkt')
 # nltk.download('stopwords')
 
+
 def preprocess_arabic_text(text):
-    # Remove non-Arabic characters and numbers
-    text = re.sub(r'[^\u0600-\u06FF\s]', '', text)
+    # Remove non-Arabic characters
+    text = re.sub(r'[^؀-ۿ\s]', '', text)
+
+    # Remove Arabic numbers
+    text = re.sub(r'[٠١٢٣٤٥٦٧٨٩]', '', text)
+
+    # Remove double quotes
+    # text = re.sub(r'[""]', '', text)
 
     # Remove repetitive characters like حككككككككزنمظخؤحسدسسدسظسز
     text = re.sub(r'(.)\1{2,}', r'\1', text)
@@ -27,8 +34,12 @@ def preprocess_arabic_text(text):
     stop_words = set(stopwords.words('arabic'))
     filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
 
+    # Apply Arabic stemming using ISRIStemmer
+    stemmer = ISRIStemmer()
+    stemmed_tokens = [stemmer.stem(word) for word in filtered_tokens]
+
     # Join the tokens back into a single string
-    preprocessed_text = ' '.join(filtered_tokens)
+    preprocessed_text = ' '.join(stemmed_tokens)
 
     return preprocessed_text
 
@@ -39,6 +50,7 @@ df = pd.read_csv(file_path)
 
 df['preprocessed_review'] = df['review_description'].apply(preprocess_arabic_text)
 
+df = df.replace('', pd.NA).dropna()
 df = df.dropna(subset=['preprocessed_review'])
 
 print("Original Data:")
@@ -46,6 +58,7 @@ print(df[['review_description']].head())
 
 print("\nPreprocessed Data:")
 print(df[['preprocessed_review']].head())
+print(df[['preprocessed_review']].tail())
 
 output_file_path = 'preprocessed_file.txt'
 
